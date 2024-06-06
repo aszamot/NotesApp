eb073c7196e306d392df198cb38ce82d20c9636b
aszamot <antkowiak.tomasz94@gmail.com>
2024-06-06 14:11:48 +0200

    Added timezone to date extensions

--- app/src/main/java/pl/atk/notes/utils/extensions/DateExtensions.kt
+++ app/src/main/java/pl/atk/notes/utils/extensions/DateExtensions.kt
@@ -1,10 +1,15 @@
 package pl.atk.notes.utils.extensions
 
+import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 fun Date.toReadableString(): String {
     val dateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+    val defaultTimeZone = TimeZone.getDefault()
+    Timber.d(defaultTimeZone.toString())
+    dateFormat.timeZone = defaultTimeZone
     return dateFormat.format(this)
 }
